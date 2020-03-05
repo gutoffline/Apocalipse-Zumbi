400035a6a1f191ad260e3ea67a0e9e062d89d59e
Guto Xavier <gutoffline@gmail.com>
2020-03-04 22:25:12 -0300

    AulA

---
+++ ApocalypseZumbi/Assets/Scripts/Bala.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+
+    public float velocidade = 100f;
+    
+    void Update()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.forward * velocidade * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider objetoColidido) {
+        Debug.Log(objetoColidido.tag);
+        if(objetoColidido.tag == "Inimigo"){
+            Destroy(objetoColidido.gameObject);
+        }
+        
+        Destroy(gameObject);
+    }
+}
